b179c6d9d5741ada9e1da5f629850dd88018cde2
Sebastian Drewes <sebas.drewes@gmail.com>
2021-06-03 18:42:30 -0300

    mejorando logica

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+const ProjectsAddTaskDisplay = (() => {
+
 const projects = [];
 //toDo factory
 const ToDo = (title, description, dueDate, priority) => {
@@ -11,18 +13,48 @@ const NewProject = (title) => {
 }
 
 const defaultProject = NewProject("Default Project");
-let pepe = ToDo("titulo", "descripcion", "fecha", "prioridad");
-let pepa = ToDo("titulo", "descripcion", "fecha", "prioridad");
-let pupu = ToDo("titulo", "descripcion", "fecha", "prioridad");
+let pepe = ToDo("pepe", "pepedescripcion", "pepefecha", "pepeprioridad");
+let pepa = ToDo("pepa", "pepadescripcion", "pepafecha", "pepaprioridad");
+let pupu = ToDo("pupu", "pupudescripcion", "pupufecha", "pupuprioridad");
 const newProject = NewProject("Otro projecto");
+
 projects.push(newProject);
 projects.push(defaultProject);
 newProject.ToDos.push(pupu);
 defaultProject.ToDos.push(pepe);
 defaultProject.ToDos.push(pepa);
+let currentProject = "";
 
+const createTarea = document.querySelector("#agregarTareas")
+createTarea.addEventListener("click", () => {
+    const ToDoTitle = prompt("titulo?")
+    const ToDoDescription = prompt("description?")
+    const ToDoFecha = prompt("duedate?")
+    const ToDoPriority = prompt("prioidad?")
+    const newToDo = ToDo(ToDoTitle, ToDoDescription, ToDoFecha, ToDoPriority)
+    const proyectosAgregados = document.querySelectorAll(".proyectosAgregados")
+    for (let i = 0; i < proyectosAgregados.length; i++) {
+        if (proyectosAgregados[i] === currentProject) {
+            for (let j = 0; j < projects.length; j++) {
+                if (proyectosAgregados[i].textContent === projects[j].title) {
+                    projects[j].ToDos.push(newToDo);
+                    proyectosAgregados[i].click();
+                }
+            }
 
-function displayProjects() {
+        }
+    }
+})
+const crearProyecto = document.querySelector("#crearProyecto")
+crearProyecto.addEventListener("click", () => {
+    const newProjectTitle = prompt("titulo?");
+    const newProyecto = NewProject(newProjectTitle);
+    projects.push(newProyecto);
+    displayProjects()
+    displayToDos()
+})
+
+const displayProjects = () => {
     const proyectos = document.querySelector("#projects");
     while (proyectos.firstChild) {
         proyectos.removeChild(proyectos.firstChild);
@@ -35,10 +67,9 @@ function displayProjects() {
     }
 }
 
-function displayToDos() {
+const displayToDos = () =>  {
     const proyectosAgregados = document.querySelectorAll(".proyectosAgregados")
     const tareas = document.querySelector("#tareas");
-
     for (let i = 0; i < proyectosAgregados.length; i++) {
         proyectosAgregados[i].addEventListener("click", () => {
             while (tareas.firstChild) {
@@ -46,6 +77,7 @@ function displayToDos() {
             }
             for (let j = 0; j < projects.length; j++) {
                 if (proyectosAgregados[i].textContent === projects[j].title) {
+                    currentProject = proyectosAgregados[i];
                     for (let l = 0; l < projects[j].ToDos.length; l++) {
                         const ToDoTitle = document.createElement("p");
                         ToDoTitle.textContent = projects[j].ToDos[l].title;
@@ -62,9 +94,10 @@ function displayToDos() {
                     }
                 }
             }
-        }
-        )
+        })
     }
 }
-displayProjects()
-displayToDos()
+return { displayProjects, displayToDos };
+})();
+ProjectsAddTaskDisplay.displayProjects();
+ProjectsAddTaskDisplay.displayToDos();
