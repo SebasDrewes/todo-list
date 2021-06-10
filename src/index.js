12b203c1b04bf89817b0e158bdae52b6c5f90523
Sebastian Drewes <sebas.drewes@gmail.com>
2021-06-10 13:18:00 -0300

    fixed some styles and commented code

--- src/index.js
+++ src/index.js
@@ -7,10 +7,13 @@ const NewProject = (title) => {
 const ToDo = (title, Nota, dueDate, priority, complete) => {
     return { title, Nota, dueDate, priority, complete };
 }
-const ProjectsAndTasks = (() => {
 
+const ProjectsAndTasks = (() => {
+    //array de proyectos
     let projects = [];
     //toDo factory
+
+    //funciones localStorage
     const saveProjects = () => {
         localStorage.setItem("projects", JSON.stringify(ProjectsAndTasks.projects))
     }
@@ -18,7 +21,7 @@ const ProjectsAndTasks = (() => {
         let storedProjects = JSON.parse(localStorage.getItem("projects"))
         if (storedProjects.length === 0) {
             const test = NewProject("Lista default")
-            const testToDo = ToDo("Test","Test nota","Mañana","Prioridad Media", false)
+            const testToDo = ToDo("Test", "Test nota", "Mañana", "Prioridad Media", false)
             test.ToDos.push(testToDo)
             ProjectsAndTasks.projects.push(test)
             Display.displayProjects();
@@ -29,7 +32,7 @@ const ProjectsAndTasks = (() => {
         }
     }
 
-
+//funcion para crear y guardar nueva tarea en array ToDos del currentProject
     const createTask = () => {
         const newTaskTitle = document.querySelector(".newTaskTitle")
         const newTaskNota = document.querySelector(".newTaskNota")
@@ -44,15 +47,14 @@ const ProjectsAndTasks = (() => {
         const ToDoFecha = newToDoFecha
         const ToDoPriority = newToDoPriority
         const ToDoComplete = false;
-       /* if (ToDoTitle.length >= 15) {
-            alert("No mas de 15 caracteres por Titulo")*/
-       /* } /*else */if (ToDoTitle === "") {
+        //si faltan datos, alerta
+        if (ToDoTitle === "") {
             alert("Falta completar campo Titulo")
-       /* } else if (ToDoNota.length >= 15) {
-            alert("No mas de 15 caracteres por Nota")*/
+
         } else if (ToDoPriority === "Prioridad") {
             alert("Por favor elige Prioridad")
         } else {
+            //loop entre todos los proyectos para seleccionar el proyecto actual, y pushea la nueva tarea a ese proyecto
             const newToDo = ToDo(ToDoTitle, ToDoNota, ToDoFecha, ToDoPriority, ToDoComplete)
             const proyectosAgregados = document.querySelectorAll(".proyectosAgregados")
             for (let i = 0; i < proyectosAgregados.length; i++) {
@@ -60,7 +62,9 @@ const ProjectsAndTasks = (() => {
                     for (let j = 0; j < ProjectsAndTasks.projects.length; j++) {
                         if (proyectosAgregados[i].textContent === ProjectsAndTasks.projects[j].title + "X") {
                             ProjectsAndTasks.projects[j].ToDos.push(newToDo);
+                            //redisplay del proyectyo actual
                             proyectosAgregados[i].click();
+                            //se se habilita eventlistener
                             const agregarTareas = document.querySelector("#agregarTareas");
                             agregarTareas.addEventListener("click", Display.crearTaskDisplay);
                         }
@@ -70,6 +74,7 @@ const ProjectsAndTasks = (() => {
             }
         }
     }
+    //funcion para crear proyecto y sumarlo a array projects
     const createProject = () => {
         const newProjectTitleSpace = document.querySelector(".newProjectTitleSpace");
         const newProjectTitle = newProjectTitleSpace.value;
@@ -78,8 +83,6 @@ const ProjectsAndTasks = (() => {
             alert("Por favor elige otro Titulo")
         } else if (newProjectTitle === "") {
             alert("Falta completar campo Titulo")
-       /* } else if (newProjectTitle.length >= 15) {
-            alert("No mas de 15 caracteres por Titulo")*/
         }
         else {
             const newProyecto = NewProject(newProjectTitle);
@@ -100,12 +103,15 @@ const ProjectsAndTasks = (() => {
 
 const Display = (() => {
     const proyectos = document.querySelector("#projects");
+    //se define currentProject como object, para poder exportarlo a funcion ProjectsAndTasks
     let currentProject = { currentProject: 0 };
     const crearProyectoDisplay = () => {
+        //si display de newTask esta abierto, lo cierra
         const newTaskCancel = document.querySelector(".newTaskCancel")
         if (newTaskCancel !== null) {
             newTaskCancel.click();
         }
+        //creacion dinamica de display Proyecto
         crearProyecto.removeEventListener("click", crearProyectoDisplay)
         const newProject = document.createElement("div");
         newProject.classList.add("newProject")
@@ -138,12 +144,16 @@ const Display = (() => {
 
 
     const crearTaskDisplay = () => {
+        // si existe proyecto y se selecciono un proyecto, se crea el display de agregarTarea
         if (ProjectsAndTasks.projects[0] !== undefined && Display.currentProject.currentProject !== 0) {
             const newProjectCancel = document.querySelector(".newProjectCancel")
+            //si el display de newProject esta abierto, lo cierra
             if (newProjectCancel !== null) {
                 newProjectCancel.click();
             }
+            //se deshabilita eventListener para que no sea posible crear mas de un display
             agregarTareas.removeEventListener("click", crearTaskDisplay);
+            //creacion dinamica displayTask
             const newTask = document.createElement("div");
             newTask.classList.add("newTask")
             const newTaskTitle = document.createElement("input");
@@ -158,11 +168,13 @@ const Display = (() => {
             newTaskFecha.classList.add("newTaskFecha");
             newTaskFecha.setAttribute("type", "text");
             newTaskFecha.setAttribute("placeholder", "Fecha limite");
+            //se agregan los siguientes attributes para poder mostrar placeholder
             newTaskFecha.setAttribute("onfocus", "(this.type='date')")
             newTaskFecha.setAttribute("onblur", "(this.type='text')")
             const newTaskPriority = document.createElement("p");
             newTaskPriority.classList.add("newTaskPriority");
             newTaskPriority.textContent = "Prioridad"
+            //eventlistener para cambiar prioridad
             newTaskPriority.addEventListener("click", () => {
                 if (newTaskPriority.textContent === "Prioridad") {
                     newTaskPriority.textContent = "Prioridad Alta"
@@ -189,6 +201,7 @@ const Display = (() => {
                 ProjectsAndTasks.saveProjects();
             })
             newTaskCancel.addEventListener("click", () => {
+                //al cancelar, se habilita addeventListener y se redisplay el proyecto actual
                 agregarTareas.addEventListener("click", crearTaskDisplay);
                 const proyectosAgregados = document.querySelectorAll(".proyectosAgregados")
                 for (let i = 0; i < proyectosAgregados.length; i++) {
@@ -215,7 +228,7 @@ const Display = (() => {
     }
     const agregarTareas = document.querySelector("#agregarTareas");
     agregarTareas.addEventListener("click", crearTaskDisplay)
-
+//display de proyectos guardados en array projects
     const displayProjects = () => {
         while (proyectos.firstChild) {
             proyectos.removeChild(proyectos.firstChild);
@@ -238,7 +251,7 @@ const Display = (() => {
             proyectoAgregado.appendChild(proyectoAgregadoDelete)
         }
     }
-
+//display de ToDos guardados en proyecto seleccionado
     const displayToDos = () => {
         const proyectosAgregados = document.querySelectorAll(".proyectosAgregados")
         const tareas = document.querySelector("#tareas");
@@ -249,16 +262,17 @@ const Display = (() => {
                     newProjectCancel.click();
                 }
                 agregarTareas.addEventListener("click", crearTaskDisplay)
-                //loop para volver al color original proyectos no seleccionados
+                //loop para volver al color original proyectos deseleccionados
                 document.querySelector("#agregarTareas").style.cssText = "visibility: visible";
                 for (let u = 0; u < proyectosAgregados.length; u++) {
                     proyectosAgregados[u].style.background = "rgb(56, 56, 56)"
                 }
-                //al proyecto seleccionado, cambia background-color*/
+                //al proyecto seleccionado, cambia background-color
                 proyectosAgregados[i].style.background = "rgb(56, 56, 56, 0.5)"
                 while (tareas.firstChild) {
                     tareas.removeChild(tareas.firstChild);
                 }
+                //creacion dinamica de cada ToDo guardado en el proyecto
                 for (let j = 0; j < ProjectsAndTasks.projects.length; j++) {
                     if (proyectosAgregados[i].textContent === (ProjectsAndTasks.projects[j].title + "X")) {
                         currentProject.currentProject = proyectosAgregados[i];
@@ -291,6 +305,7 @@ const Display = (() => {
                             ToDoComplete.classList.add("ToDoComplete");
                             ToDoComplete.textContent = "Completar"
                             ToDoComplete.style.cssText = "cursor: pointer"
+                            //add event listener activar/desactivar completar y cambiar estilos del ToDo
                             ToDoComplete.addEventListener("click", () => {
                                 if (ProjectsAndTasks.projects[j].ToDos[l].complete === true) {
                                     ProjectsAndTasks.projects[j].ToDos[l].complete = false;
@@ -335,6 +350,7 @@ const Display = (() => {
     }
     return { displayProjects, displayToDos, currentProject, crearProyectoDisplay, crearTaskDisplay };
 })();
+//funciones para mostrar projectos y tareas guardadas
 ProjectsAndTasks.loadProjects();
 Display.displayProjects();
 Display.displayToDos();
